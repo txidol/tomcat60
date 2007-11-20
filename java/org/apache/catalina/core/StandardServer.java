7cfe85d4dceda52e027aafc34a493d66b4ef4788
Filip Hanik <fhanik@apache.org>
2007-11-20 22:53:59 +0000

    Fix compilation error
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@596865 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardServer.java
+++ java/org/apache/catalina/core/StandardServer.java
@@ -372,7 +372,7 @@ public final class StandardServer
         try {
             serverSocket =
                 new ServerSocket(port, 1,
-                                 InetAddress.getByName("localhost").getHostAddress());
+                                 InetAddress.getByName("localhost"));
         } catch (IOException e) {
             log.error("StandardServer.await: create[" + port
                                + "]: ", e);
