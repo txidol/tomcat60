e2a413fa613349b9fe777453174441818069fcdc
Mark Emlyn David Thomas <markt@apache.org>
2008-09-19 10:59:38 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=45785 Ignore directories named xxx.jar in WEB-INF/lib
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@697048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/loader/WebappLoader.java
+++ java/org/apache/catalina/loader/WebappLoader.java
@@ -998,7 +998,13 @@ public class WebappLoader
                     log.debug(sm.getString("webappLoader.jarDeploy", filename,
                                      destFile.getAbsolutePath()));
 
-                    Resource jarResource = (Resource) binding.getObject();
+                    Object obj = binding.getObject();
+                    
+                    if (!(obj instanceof Resource))
+                        continue;
+                    
+                    Resource jarResource = (Resource) obj;
+                    
                     if (copyJars) {
                         if (!copy(jarResource.streamContent(),
                                   new FileOutputStream(destFile)))
