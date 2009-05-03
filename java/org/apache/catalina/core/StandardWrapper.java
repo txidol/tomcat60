adefe754f46f0c0df35cf806921f867bce083fec
Mark Emlyn David Thomas <markt@apache.org>
2009-05-03 01:33:01 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47013 Use system property rather than hard code default
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@771014 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardWrapper.java
+++ java/org/apache/catalina/core/StandardWrapper.java
@@ -1178,7 +1178,7 @@ public class StandardWrapper
                     // Invoking jspInit
                     DummyRequest req = new DummyRequest();
                     req.setServletPath(jspFile);
-                    req.setQueryString("jsp_precompile=true");
+                    req.setQueryString(Constants.PRECOMPILE + "=true");
                     DummyResponse res = new DummyResponse();
 
                     if( Globals.IS_SECURITY_ENABLED) {
