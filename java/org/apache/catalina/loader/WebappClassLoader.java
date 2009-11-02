6e91a99de422f63581e215ab337a3656952d5fa1
Mark Emlyn David Thomas <markt@apache.org>
2009-11-02 23:41:54 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47987 Limit size of not found resources cache
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@832200 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/loader/WebappClassLoader.java
+++ java/org/apache/catalina/loader/WebappClassLoader.java
@@ -40,6 +40,8 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Vector;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
@@ -243,7 +245,14 @@ public class WebappClassLoader
     /**
      * The list of not found resources.
      */
-    protected HashMap notFoundResources = new HashMap();
+    protected HashMap<String, String> notFoundResources =
+        new LinkedHashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        protected boolean removeEldestEntry(
+                Map.Entry<String, String> eldest) {
+            return size() > 1000;
+        }
+    };
 
 
     /**
