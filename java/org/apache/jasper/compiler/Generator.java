6f830836d1d9428648af3b2f5016e67b5fcbb2bb
Mark Emlyn David Thomas <markt@apache.org>
2010-04-11 13:44:50 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48701 Add system property to allow disabling enforcement of JSP.5.3. The spec recommends, but does not require this enforcement. (kkolinko)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@932898 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Generator.java
+++ java/org/apache/jasper/compiler/Generator.java
@@ -78,6 +78,15 @@ class Generator {
     private static final String VAR_ANNOTATIONPROCESSOR = 
         System.getProperty("org.apache.jasper.compiler.Generator.VAR_ANNOTATIONPROCESSOR", "_jsp_annotationprocessor");
 
+    /* System property that controls if the requirement to have the object
+     * used in jsp:getProperty action to be previously "introduced"
+     * to the JSP processor (see JSP.5.3) is enforced.
+     */ 
+    private static final boolean STRICT_GET_PROPERTY = Boolean.valueOf(
+            System.getProperty(
+                    "org.apache.jasper.compiler.Generator.STRICT_GET_PROPERTY",
+                    "true")).booleanValue();
+
     private ServletWriter out;
 
     private ArrayList methodsBuffered;
@@ -1064,7 +1073,7 @@ class Generator {
                                 + ")_jspx_page_context.findAttribute("
                                 + "\""
                                 + name + "\"))." + methodName + "())));");
-            } else if (varInfoNames.contains(name)) {
+            } else if (!STRICT_GET_PROPERTY || varInfoNames.contains(name)) {
                 // The object is a custom action with an associated
                 // VariableInfo entry for this name.
                 // Get the class name and then introspect at runtime.
