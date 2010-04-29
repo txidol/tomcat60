f7c9ffd318ed9aeac55680e517208071f2eed23f
Konstantin Kolinko <kkolinko@apache.org>
2010-04-29 15:36:29 +0000

    CTR Replace $Date$ keyword with $Id$ in JavaDoc comments, because of its i18n and timezone issues (http://subversion.tigris.org/issues/show_bug.cgi?id=2332)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@939350 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/deploy/ContextService.java
+++ java/org/apache/catalina/deploy/ContextService.java
@@ -29,7 +29,7 @@ import java.util.HashMap;
  * deployment descriptor.
  *
  * @author Fabien Carrion
- * @version $Revision$ $Date$
+ * @version $Id$
  */
 
 public class ContextService extends ResourceBase implements Serializable {
