74511f76a8fd7c4f8c3acbebf7bd11767d24927b
Konstantin Kolinko <kkolinko@apache.org>
2010-06-01 16:16:34 +0000

    Code cleanup in o.a.t.util.buf classes It is port of r481614 from tc5.5.x
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@950142 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/StringCache.java
+++ java/org/apache/tomcat/util/buf/StringCache.java
@@ -257,8 +257,6 @@ public class StringCache {
                         while (n < size) {
                             Object key = tempMap.lastKey();
                             ArrayList list = (ArrayList) tempMap.get(key);
-                            ByteEntry[] list2 = 
-                                (ByteEntry[]) list.toArray(new ByteEntry[list.size()]);
                             for (int i = 0; i < list.size() && n < size; i++) {
                                 ByteEntry entry = (ByteEntry) list.get(i);
                                 tempChunk.setBytes(entry.name, 0, entry.name.length);
@@ -371,8 +369,6 @@ public class StringCache {
                         while (n < size) {
                             Object key = tempMap.lastKey();
                             ArrayList list = (ArrayList) tempMap.get(key);
-                            CharEntry[] list2 = 
-                                (CharEntry[]) list.toArray(new CharEntry[list.size()]);
                             for (int i = 0; i < list.size() && n < size; i++) {
                                 CharEntry entry = (CharEntry) list.get(i);
                                 tempChunk.setChars(entry.name, 0, entry.name.length);
