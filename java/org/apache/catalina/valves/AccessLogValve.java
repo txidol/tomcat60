89912823c5df8cb676987637b4b348bf3b6f2eb2
Mark Emlyn David Thomas <markt@apache.org>
2011-04-18 14:21:58 +0000

    Implement display of multiple request headers in AccessLogValve (kkolinko)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1094587 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/valves/AccessLogValve.java
+++ java/org/apache/catalina/valves/AccessLogValve.java
@@ -28,6 +28,7 @@ import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -1213,12 +1214,15 @@ public class AccessLogValve extends ValveBase implements AccessLog, Lifecycle {
 
         public void addElement(StringBuffer buf, Date date, Request request,
                 Response response, long time) {
-            String value = request.getHeader(header);
-            if (value == null) {
-                buf.append('-');
-            } else {
-                buf.append(value);
+            Enumeration<String> iter = request.getHeaders(header);
+            if (iter.hasMoreElements()) {
+                buf.append(iter.nextElement());
+                while (iter.hasMoreElements()) {
+                    buf.append(',').append(iter.nextElement());
+                }
+                return;
             }
+            buf.append('-');
         }
     }
 
