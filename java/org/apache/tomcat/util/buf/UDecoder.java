b0aa367e5b83e54a6b9cc48228e83da3e17d151b
Konstantin Kolinko <kkolinko@apache.org>
2012-03-15 21:47:30 +0000

    - Align %2f handling (aka CVE-2007-0450 fix) between implementations of UDecoder.convert(). That is: reject '%2f' if Tomcat is configured to do so. - convert(MessageBytes): Make it to consistently throw an IOException on decoding errors, regardless of what is behind the MessageBytes facade. If there was a String it was re-throwing RuntimeException from convert(String). - convert(String): Use StringBuilder
    convert(String) and convert(CharChunk) are rare cases with little impact
    (and inherent bug: %xx are converted from chars to chars, effectively treating them as ISO-8859-1 regardless of request encoding).
    
    It is merge of r1203091 from trunk.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1301221 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/UDecoder.java
+++ java/org/apache/tomcat/util/buf/UDecoder.java
@@ -93,7 +93,7 @@ public final class UDecoder {
             idx=idx2;
         }
 
-        boolean noSlash = !(ALLOW_ENCODED_SLASH || query);
+        final boolean noSlash = !(ALLOW_ENCODED_SLASH || query);
 
         for( int j=idx; j<end; j++, idx++ ) {
             if( buff[ j ] == '+' && query) {
@@ -160,6 +160,8 @@ public final class UDecoder {
             idx=idx2;
         }
 
+        final boolean noSlash = !(ALLOW_ENCODED_SLASH || query);
+
         for( int j=idx; j<cend; j++, idx++ ) {
             if( buff[ j ] == '+' && query ) {
                 buff[idx]=( ' ' );
@@ -178,6 +180,9 @@ public final class UDecoder {
                 
                 j+=2;
                 int res=x2c( b1, b2 );
+                if (noSlash && (res == '/')) {
+                    throw EXCEPTION_SLASH;
+                }
                 buff[idx]=(char)res;
             }
         }
@@ -203,7 +208,11 @@ public final class UDecoder {
         case MessageBytes.T_STR:
             String strValue=mb.toString();
             if( strValue==null ) return;
-            mb.setString( convert( strValue, query ));
+            try {
+                mb.setString( convert( strValue, query ));
+            } catch (RuntimeException ex) {
+                throw new DecodeException(ex.getMessage());
+            }
             break;
         case MessageBytes.T_CHARS:
             CharChunk charC=mb.getCharChunk();
@@ -229,8 +238,10 @@ public final class UDecoder {
         
         if( (!query || str.indexOf( '+' ) < 0) && str.indexOf( '%' ) < 0 )
             return str;
-        
-        StringBuffer dec = new StringBuffer();    // decoded string output
+
+        final boolean noSlash = !(ALLOW_ENCODED_SLASH || query);
+
+        StringBuilder dec = new StringBuilder();    // decoded string output
         int strPos = 0;
         int strLen = str.length();
 
@@ -267,8 +278,12 @@ public final class UDecoder {
                 // We throw the original exception - the super will deal with
                 // it
                 //                try {
-                dec.append((char)Integer.
-                           parseInt(str.substring(strPos + 1, strPos + 3),16));
+                char res = (char) Integer.parseInt(
+                        str.substring(strPos + 1, strPos + 3), 16);
+                if (noSlash && (res == '/')) {
+                    throw new IllegalArgumentException("noSlash");
+                }
+                dec.append(res);
                 strPos += 3;
             }
         }
