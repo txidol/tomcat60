fb072a2003bbb823fd585912ffef82e46fd88a7e
Christopher Schultz <schultz@apache.org>
2012-08-09 18:14:14 +0000

    https://issues.apache.org/bugzilla/show_bug.cgi?id=53531 Fix ExpandWar.expand to check the return value of File.mkdir and File.mkdirs
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@1371355 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/ExpandWar.java
+++ java/org/apache/catalina/startup/ExpandWar.java
@@ -132,7 +132,8 @@ public class ExpandWar {
         }
 
         // Create the new document base directory
-        docBase.mkdir();
+        if(!docBase.mkdir() && !docBase.isDirectory())
+            throw new IOException(sm.getString("expandWar.createFailed", docBase));
 
         // Expand the WAR into the new document base directory
         String canonicalDocBasePrefix = docBase.getCanonicalPath();
@@ -162,7 +163,9 @@ public class ExpandWar {
                 if (last >= 0) {
                     File parent = new File(docBase,
                                            name.substring(0, last));
-                    parent.mkdirs();
+
+                    if(!parent.mkdirs() && !parent.isDirectory())
+                        throw new IOException(sm.getString("expandWar.createFailed", parent));
                 }
                 if (name.endsWith("/")) {
                     continue;
